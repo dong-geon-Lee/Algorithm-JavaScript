e7abf8289e90177e57f47bd215e2d15dc6360add
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-08-30 13:44:54 +0900

    completed 05

--- 2-1,2차원 배열 탐색/05. 등수구하기/script.js
+++ 2-1,2차원 배열 탐색/05. 등수구하기/script.js
@@ -1,14 +1,41 @@
-// 문제:
+// ! 솔루션 1
+function mySolution(arr) {
+  let n = arr.length;
+  let answer = Array.from({ length: n }, () => 1);
 
-function mySolution(str) {
-  // 코드를 넣으세요
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[j] > arr[i]) ++answer[i];
+    }
+  }
+
+  console.log(answer);
 }
 
-let str = "";
+let str = [80, 20, 50, 60, 100];
 console.log(mySolution(str));
 
-function solution(s) {
-  // 코드를 넣으세요
+// ! 솔루선 2
+function solution1(arr) {
+  console.log(arr);
+  const arr_ = arr.map((i) => console.log(arr.filter((j) => i < j).length + 1));
+
+  arr.map((i) => arr.filter((j) => i < j).length);
+
+  console.log(arr_);
 }
 
-console.log(solution(str));
+// ! 솔루선 3
+function solution2(arr) {
+  const answer = [];
+
+  const copy = arr.slice();
+
+  arr.sort((a, b) => b - a);
+
+  for (let i = 0; i < arr.length; i++) {
+    answer.push(arr.indexOf(copy[i]) + 1);
+  }
+
+  return answer;
+}
