e63187e1897a6b134247b047d409066f288bfa46
dong-geon-Lee <ch9716@naver.com>
2023-04-02 21:17:01 +0900

    회문 문자열 문제 풀이 완료

--- 3-문자열 탐색/01. 회문 문자열/script.js
+++ 3-문자열 탐색/01. 회문 문자열/script.js
@@ -1,14 +1,38 @@
-// 문제:
+// 문제: 회문 문자열
 
 function mySolution(str) {
-  // 코드를 넣으세요
+  let words = str.slice().toLowerCase();
+  let newWords = "";
+  for (let i = words.length - 1; i >= 0; i--) {
+    newWords += words[i];
+  }
+
+  if (words === newWords) return "YES";
+  else return "NO";
 }
 
-let str = "";
+let str = "gooG";
 console.log(mySolution(str));
 
 function solution(s) {
-  // 코드를 넣으세요
+  let answer = "YES";
+  s = s.toLowerCase();
+  let len = s.length;
+
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s[i] !== s[len - i - 1]) return "NO";
+  }
+
+  return answer;
 }
 
 console.log(solution(str));
+
+function solution2(s) {
+  let answer = "YES";
+  s = s.toLowerCase();
+  if (s.split("").reverse().join("") !== s) return "NO";
+  return answer;
+}
+
+console.log(solution2(str));
