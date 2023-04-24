39fffd8a4791e4d41e25ae917e1189f217c7abf7
dong-geon-Lee <ch9716@naver.com>
2023-04-24 17:58:36 +0900

    수 조작하기 1

---
+++ 2-programmers-algorithm/수 조작하기 1/script.js
@@ -0,0 +1,23 @@
+function solution(n, control) {
+  for (let i = 0; i < control.length; i++) {
+    switch (control[i]) {
+      case "w":
+        n++;
+        break;
+      case "s":
+        n--;
+        break;
+      case "d":
+        n += 10;
+        break;
+      case "a":
+        n -= 10;
+        break;
+    }
+  }
+  return n;
+}
+
+console.log(solution(0, "wsdawsdassw"));
+
+// *good
