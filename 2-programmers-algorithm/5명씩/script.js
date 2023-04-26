d670863ea00ca2e2b15622a49ec29321a27d11a5
dong-geon-Lee <ch9716@naver.com>
2023-04-26 21:23:13 +0900

    5명씩

---
+++ 2-programmers-algorithm/5명씩/script.js
@@ -0,0 +1,18 @@
+function solution(names) {
+  return names.filter((_, i) => !(i % 5));
+}
+
+console.log(
+  solution([
+    "nami",
+    "ahri",
+    "jayce",
+    "garen",
+    "ivern",
+    "vex",
+    "jinx",
+    "a",
+    "b",
+    "c",
+  ])
+);
