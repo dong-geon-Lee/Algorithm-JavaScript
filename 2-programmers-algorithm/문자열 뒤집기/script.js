d3cdcb4d4e7376d19078d6bd0c6ddcb88a9bf48a
dong-geon-Lee <ch9716@naver.com>
2023-04-26 17:47:03 +0900

    문자열 뒤집기

---
+++ 2-programmers-algorithm/문자열 뒤집기/script.js
@@ -0,0 +1,13 @@
+function solution(my_string, s, e) {
+  return (
+    my_string.slice(0, s) +
+    my_string
+      .slice(s, e + 1)
+      .split("")
+      .reverse()
+      .join("") +
+    my_string.slice(e + 1)
+  );
+}
+
+console.log(solution("Progra21Sremm3", 6, 12));
