2e8bc732d92ab7e2b87062202233d40c88ebdc6d
dong-geon-Lee <ch9716@naver.com>
2023-04-28 12:21:09 +0900

    배열 만들기 5

---
+++ 2-programmers-algorithm/배열 만들기 5/script.js
@@ -0,0 +1,7 @@
+function solution(intStrs, k, s, l) {
+  return intStrs.map((x) => +x.slice(s, s + l)).filter((x) => x > k);
+}
+
+console.log(
+  solution(["0123456789", "9876543210", "9999999999999"], 50000, 5, 5)
+);
