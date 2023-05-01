09671507189844f4a330773250cd0063ed877582
dong-geon-Lee <ch9716@naver.com>
2023-05-01 09:54:32 +0900

    Staircase

--- 3-hackerRank/Staircase/script.js
+++ 3-hackerRank/Staircase/script.js
@@ -1,9 +1,6 @@
 function solution(n) {
   for (let i = 1; i <= n; i++) {
-    let spaces = n - i;
-    let hashes = i;
-    let row = " ".repeat(spaces) + "#".repeat(hashes);
-    console.log(row);
+    console.log(" ".repeat(n - i) + "#".repeat(i));
   }
 }
 
