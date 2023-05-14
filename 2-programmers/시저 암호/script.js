b493ec96a1f52fa618160e2ccd1c98adc9d6d55e
dong-geon-Lee <ch9716@naver.com>
2023-05-14 19:54:29 +0900

    시저 암호

---
+++ 2-programmers/시저 암호/script.js
@@ -0,0 +1,23 @@
+function solution(s, n) {
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    let charCode = s.charCodeAt(i);
+    let shiftedCode;
+
+    console.log(charCode);
+    if (charCode >= 65 && charCode <= 90) {
+      shiftedCode = ((charCode - 65 + n) % 26) + 65;
+    } else if (charCode >= 97 && charCode <= 122) {
+      shiftedCode = ((charCode - 97 + n) % 26) + 97;
+    } else {
+      shiftedCode = charCode;
+    }
+
+    result += String.fromCharCode(shiftedCode);
+  }
+
+  return result;
+}
+
+console.log(solution("a B z", 4));
