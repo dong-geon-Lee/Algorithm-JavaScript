007662650629b13b4163ca464c4adb3d0a69d5ad
dong-geon-Lee <ch9716@naver.com>
2023-05-17 13:36:19 +0900

    Left Rotation

--- 3-hackerRank/Left Rotation/script.js
+++ 3-hackerRank/Left Rotation/script.js
@@ -1,16 +1,10 @@
-function solution(s) {
-  const isPM = s.indexOf("PM") !== -1;
-  const isAM = s.indexOf("AM") !== -1;
+function rotateLeft(d, arr) {
+  for (let i = 0; i < d; i++) {
+    const el = arr.shift();
+    arr.push(el);
+  }
 
-  let hour = parseInt(s.slice(0, 2));
-  let minute = s.slice(3, 5);
-  let second = s.slice(6, 8);
-
-  if (isPM && hour !== 12) hour += 12;
-  else if (isAM && hour === 12) hour = 0;
-
-  hour = hour < 10 ? "0" + hour : hour;
-  return `${hour}:${minute}:${second}`;
+  return arr;
 }
 
-console.log(solution("04:59:59AM"));
+console.log(rotateLeft(4, [1, 2, 3, 4, 5]));
