4140cff4ef4939ae4250d292c823482d832c3fdf
dong-geon-Lee <ch9716@naver.com>
2023-05-19 20:21:48 +0900

    Mars Exploration

--- 3-hackerRank/Mars Exploration/script.js
+++ 3-hackerRank/Mars Exploration/script.js
@@ -1,32 +1,19 @@
-function alternate(s) {
-  const distinctChars = [...new Set(s)];
-  let maxValidLength = 0;
+function marsExploration(s) {
+  let alteredCount = 0;
 
-  for (let i = 0; i < distinctChars.length - 1; i++) {
-    for (let j = i + 1; j < distinctChars.length; j++) {
-      const a = distinctChars[i];
-      const b = distinctChars[j];
-
-      const filteredString = s
-        .split("")
-        .filter((char) => char === a || char === b);
-
-      if (isAlternate(filteredString)) {
-        const currentLength = filteredString.length;
-        maxValidLength = Math.max(maxValidLength, currentLength);
-      }
+  for (let i = 0; i < s.length; i += 3) {
+    if (s[i] !== "S") {
+      alteredCount++;
+    }
+    if (s[i + 1] !== "O") {
+      alteredCount++;
+    }
+    if (s[i + 2] !== "S") {
+      alteredCount++;
     }
   }
 
-  return maxValidLength;
+  return alteredCount;
 }
 
-function isAlternate(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] === arr[i + 1]) return false;
-  }
-  return true;
-}
-
-console.log(alternate("beabeefeab"));
-// babab
+console.log(marsExploration("SOS SOS SSS OSO IEI SOS POI SOS IUS ISO")); // 14
