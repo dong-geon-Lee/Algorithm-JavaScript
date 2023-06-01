d0fb30634196307b57465a85af910dfffc0e842c
dong-geon-Lee <ch9716@naver.com>
2023-06-01 14:25:00 +0900

    배열 만들기 6

--- 2-programmers/배열 만들기 6/script.js
+++ 2-programmers/배열 만들기 6/script.js
@@ -1,10 +1,28 @@
-function solution(A, B) {
-  if (A === B) return 0;
+function solution(arr) {
+  // let stk = [];
+  // let condition = stk.length > 0 && stk[stk.length - 1];
 
-  let str = "";
-  let i = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (stk.length === 0) {
+  //     stk.push(arr[i]);
+  //   } else if (condition === arr[i]) {
+  //     stk.pop();
+  //   } else if (condition !== arr[i]) {
+  //     stk.push(arr[i]);
+  //   }
+  // }
 
-  return str.slice(-1) + str.slice(0, A.length - 1) === B ? 1 : -1;
+  // return stk.length === 0 ? [-1] : stk;
+
+  let stk = [];
+
+  arr.forEach((x) => {
+    if (x !== stk[stk.length - 1]) stk.push(x);
+    else stk.splice(-1);
+  });
+
+  if (stk.length === 0) stk.push([-1]);
+  return stk;
 }
 
-console.log(solution("hello", "ohell"));
+console.log(solution([0, 1, 1, 1, 0]));
