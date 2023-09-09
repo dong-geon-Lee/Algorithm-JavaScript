e4e0380f964d0dbcd6235b10d2b0222b57f8346b
dong-geon-Lee <ch9716@naver.com>
2023-09-09 11:20:58 +0900

    배열 조각하기

---
+++ 2-programmers/배열 조각하기/script.js
@@ -0,0 +1,15 @@
+function solution(arr, query) {
+  for (let i = 0; i < query.length; i++) {
+    if (i % 2 === 0) {
+      arr = arr.slice(0, query[i] + 1);
+    } else {
+      arr = arr.slice(query[i]);
+    }
+  }
+
+  return arr;
+}
+
+const arr = [0, 1, 2, 3, 4, 5];
+const query = [4, 1, 2];
+console.log(solution(arr, query));
