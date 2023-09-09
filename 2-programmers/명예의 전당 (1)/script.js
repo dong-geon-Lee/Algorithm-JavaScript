3bed8eeab8fc5eb21a688fb10aefa7a343f10b3c
dong-geon-Lee <ch9716@naver.com>
2023-09-09 12:19:13 +0900

    명예의 전당 (1)

--- 2-programmers/명예의 전당 (1)/script.js
+++ 2-programmers/명예의 전당 (1)/script.js
@@ -1,6 +1,32 @@
 function solution(k, score) {
-  console.log(k, score);
+  // * 내가 짠 코드
+  // let arr = [];
+  // let results = [];
+
+  // for (let i = 0; i < score.length; i++) {
+  //   if (i < k) {
+  //     arr.push(score[i]);
+  //     results.push(Math.min(...arr));
+  //   } else {
+  //     const idx = arr.findIndex((a) => a === Math.min(...arr));
+  //     if (Math.min(...arr) > score[i]) {
+  //     } else {
+  //       arr[idx] = score[i];
+  //     }
+  //     results.push(Math.min(...arr));
+  //   }
+  // }
+
+  // return results;
+
+  // * 배우고 싶은 코드
+  let answer = [];
+
+  return score.reduce((acc, cur) => {
+    answer.push(cur);
+    answer = answer.sort((a, b) => b - a).slice(0, k);
+    return [...acc, Math.min(...answer)];
+  }, []);
 }
 
 console.log(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]));
-// [0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
