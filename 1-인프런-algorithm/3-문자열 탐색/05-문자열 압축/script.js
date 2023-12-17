a7d20bdf5b59e687b0ff8fc28d8bb6ed825091d5
developer <ch9716@naver.com>
2023-12-17 18:35:51 +0900

    문자열 압축

--- 1-인프런-algorithm/3-문자열 탐색/05-문자열 압축/script.js
+++ 1-인프런-algorithm/3-문자열 탐색/05-문자열 압축/script.js
@@ -1,14 +1,53 @@
 // 문제:
 
-function mySolution(str) {
-  // 코드를 넣으세요
+function solutionMe(str) {
+  const answers = [];
+  const keywords = [...new Set(str)];
+  const counts = keywords.slice().fill(0);
+
+  for (let i = 0; i < keywords.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (keywords[i] === str[j]) counts[i]++;
+    }
+    if (counts[i] === 1) answers.push(keywords[i]);
+    else answers.push(keywords[i] + counts[i].toString());
+  }
+
+  return answers.join("");
 }
 
-let str = "";
-console.log(mySolution(str));
+function solutionGPT(str) {
+  let result = "";
+  let count = 1;
+  str += " ";
 
-function solution(s) {
-  // 코드를 넣으세요
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === str[i + 1]) count++;
+    else {
+      result += str[i];
+      if (count > 1) result += count;
+      count = 1;
+    }
+  }
+
+  return result;
 }
 
-console.log(solution(str));
+function solutionCourse(s) {
+  let answer = "";
+  let cnt = 1;
+  s = s + " ";
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) cnt++;
+    else {
+      answer += s[i];
+      if (cnt > 1) answer += String(cnt);
+      cnt = 1;
+    }
+  }
+}
+
+let str = "KKHSSSSSSSE";
+console.log(solutionMe(str));
+console.log(solutionGPT(str));
+console.log(solutionCourse(str));
