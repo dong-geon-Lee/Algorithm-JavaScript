de59ff6e37fe0b376948993f0944f8152bf0669d
developer <ch9716@naver.com>
2023-12-17 16:46:09 +0900

    알고리즘 수정 및 초기세팅

---
+++ 1-인프런-algorithm/3-문자열 탐색/05-문자열 압축/script.js
@@ -0,0 +1,14 @@
+// 문제:
+
+function mySolution(str) {
+  // 코드를 넣으세요
+}
+
+let str = "";
+console.log(mySolution(str));
+
+function solution(s) {
+  // 코드를 넣으세요
+}
+
+console.log(solution(str));
---
+++ 2-programmers/소수 찾기/script.js
@@ -0,0 +1,5 @@
+function solution(n) {
+  console.log(n);
+}
+
+console.log(solution(10));
