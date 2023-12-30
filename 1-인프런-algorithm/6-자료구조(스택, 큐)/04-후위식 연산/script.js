9e9161cd34b9f1d5d305b4ebad6fa8dc38f52d8b
developer <ch9716@naver.com>
2023-12-30 10:43:30 +0900

    인프런, 알고리즘 문제 세팅

---
+++ 1-인프런-algorithm/6-자료구조(스택, 큐)/04-후위식 연산/script.js
@@ -0,0 +1,18 @@
+// 문제:
+
+function solutionMe(str) {
+  // 내 코드
+}
+
+function solutionGPT(str) {
+  // GPT 코드
+}
+
+function solutionCourse(str) {
+  // 강의 코드
+}
+
+let str = "";
+console.log(solutionMe(str));
+console.log(solutionGPT(str));
+console.log(solutionCourse(str));
