2dea189af006abbb0ff1ab92f9f9d1616ca3bd36
developer <ch9716@naver.com>
2024-01-06 19:22:49 +0900

    최소, 최대

--- 4-Back-Joon/4단계/3-10818/correct.js
+++ 4-Back-Joon/4단계/3-10818/correct.js
@@ -3,7 +3,11 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n");
 
 function solution(input) {
-  let [n, arr, t] = input;
+  let [_, arr] = input;
+  let min = Math.min(...arr.split(" "));
+  let max = Math.max(...arr.split(" "));
+
+  return min.toString().concat(" ", max);
 }
 
 console.log(solution(input));
