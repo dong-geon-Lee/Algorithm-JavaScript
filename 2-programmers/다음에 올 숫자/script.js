adaccc88bd916069853e5802d7c9373466fefcf9
developer <ch9716@naver.com>
2024-04-03 10:19:36 +0900

    다음에 올 숫자

--- 2-programmers/다음에 올 숫자/script.js
+++ 2-programmers/다음에 올 숫자/script.js
@@ -1,5 +1,10 @@
-function solution(n) {
-  console.log(n);
+function solution(arr) {
+  if (arr[1] - arr[0] === arr[2] - arr[1]) {
+    return arr[arr.length - 1] + arr[1] - arr[0];
+  } else {
+    return arr[arr.length - 1] * (arr[1] / arr[0]);
+  }
 }
 
 solution([1, 2, 3, 4]);
+solution([2, 4, 8]);
