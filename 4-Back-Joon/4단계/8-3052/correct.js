cf567d89266d8eaf7381dd0c85cce80b6dee0ac9
developer <ch9716@naver.com>
2024-04-16 12:49:11 +0900

    백준 8-3052

--- 4-Back-Joon/4단계/8-3052/correct.js
+++ 4-Back-Joon/4단계/8-3052/correct.js
@@ -3,13 +3,18 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs
   .readFileSync(filePath)
   .toString()
+  .trim()
   .split("\n")
   .map((x) => +x);
 
 function solution(arr) {
-  const answer = [];
-  arr.forEach((x) => answer.push(x % 42));
-  return [...new Set(answer)].length;
+  const results = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    results.push(arr[i] % 42);
+  }
+
+  return [...new Set(results)].length;
 }
 
 console.log(solution(input));
