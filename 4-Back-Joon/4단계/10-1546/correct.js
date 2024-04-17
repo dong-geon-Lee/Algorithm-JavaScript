058c38e07220f1d264ad177d7744f071d9357a38
developer <ch9716@naver.com>
2024-04-17 10:18:20 +0900

    백준 10-1546 풀이

--- 4-Back-Joon/4단계/10-1546/correct.js
+++ 4-Back-Joon/4단계/10-1546/correct.js
@@ -3,17 +3,16 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n");
 
 function solution(input) {
-  const [n, arr] = input;
-  const data = arr.split(" ").map((a) => +a);
-  const sum = data.reduce((acc, cur) => acc + cur, 0);
-  const rest = sum / Math.max(...data);
-  const results = (rest * 100) / n;
+  const subjectCnt = +input[0];
+  const scores = input[1].split(" ").map(Number);
+  const maxScore = Math.max(...scores);
+  let total = 0;
 
-  if (results.toString().split(".").length === 1) {
-    console.log(results.toFixed(1));
-  } else {
-    console.log(results);
+  for (let i = 0; i < subjectCnt; i++) {
+    total += (scores[i] / maxScore) * 100;
   }
+
+  return total / subjectCnt;
 }
 
 console.log(solution(input));
