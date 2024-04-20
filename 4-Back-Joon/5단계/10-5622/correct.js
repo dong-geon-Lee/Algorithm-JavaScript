b6cce0c9599cc3704430e9a47edd19550442c88c
developer <ch9716@naver.com>
2024-04-20 11:34:04 +0900

    백준 10-5622

---
+++ 4-Back-Joon/5단계/10-5622/correct.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+function solution(str) {
+  const obj = {
+    A: 2,
+    B: 2,
+    C: 2,
+    D: 3,
+    E: 3,
+    F: 3,
+    G: 4,
+    H: 4,
+    I: 4,
+    J: 5,
+    K: 5,
+    L: 5,
+    M: 6,
+    N: 6,
+    O: 6,
+    P: 7,
+    Q: 7,
+    R: 7,
+    S: 7,
+    T: 8,
+    U: 8,
+    V: 8,
+    W: 9,
+    X: 9,
+    Y: 9,
+    Z: 9,
+  };
+
+  let total = 0;
+  for (let i = 0; i < str.length; i++) {
+    total += obj[str[i]];
+  }
+
+  return total + str.length;
+}
+
+console.log(solution(input));
