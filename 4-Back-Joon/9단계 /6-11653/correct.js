00be7f6decd49d89ac22267df274a816cafba42b
developer <ch9716@naver.com>
2024-04-30 15:17:41 +0900

    백준 6-11653

---
+++ 4-Back-Joon/9단계 /6-11653/correct.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+function solution(n) {
+  const factors = [];
+  let divisor = 2;
+
+  while (n > 1) {
+    while (n % divisor === 0) {
+      factors.push(divisor);
+      n /= divisor;
+    }
+    divisor++;
+  }
+
+  return factors.join("\n");
+}
+
+console.log(solution(input));
