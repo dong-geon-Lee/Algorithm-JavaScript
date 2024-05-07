b1e21bcbe29aab03ea5d3ad414076eb836fe3132
developer <ch9716@naver.com>
2024-05-07 13:47:12 +0900

    백준 1181(수정)

--- 0-Back-Joon/solve.ac/2-class-two/1181번/correct.js
+++ 0-Back-Joon/solve.ac/2-class-two/1181번/correct.js
@@ -4,7 +4,6 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 function solution(input) {
   let arr = [...new Set(input.slice(1))];
-  console.log(arr);
   let sortedWords = arr.sort((a, b) => {
     if (a.length === b.length) return a.localeCompare(b);
     return a.length - b.length;
